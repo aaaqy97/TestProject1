0d6cf5c906ee460b3dc45b410d45eeebac966d59
jiuhenbang111 <1157854019@qq.com>
2019-07-25 19:24:14 +0800

    新加了dao

---
+++ src/main/java/com/aaa/test/dao/EmpDao.java
@@ -0,0 +1,13 @@
+package com.aaa.test.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface EmpDao {
+
+    /**
+     * 获取员工列表
+     * @return
+     */
+    List<Map> getListA();
+}

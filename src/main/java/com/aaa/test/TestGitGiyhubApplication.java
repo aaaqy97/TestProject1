4ec29bdc3fe5a42e3631f34c75c2b21f46ff0c03
jiuhenbang111 <1157854019@qq.com>
2019-07-25 19:14:10 +0800

    第一次提交到本地仓库,必须填写提交备注，否则不让提交

---
+++ src/main/java/com/aaa/test/TestGitGiyhubApplication.java
@@ -0,0 +1,13 @@
+package com.aaa.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitGiyhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGitGiyhubApplication.class, args);
+	}
+
+}
